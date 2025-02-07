514ae608c83379cbcca30629759211dbfcb9a12c
Kenneth S. Reyes <kenneth.rejes@gmail.com>
2025-02-07 15:26:22 +0100

    add `fsi` dataset

---
+++ R/fsi.R
@@ -0,0 +1,24 @@
+#' Fragile States Index
+#'
+#' The Fragile States Index (FSI) is produced by The Fund for Peace (FFP) and
+#' provides a measure of state pressure and the sources of pressure
+#'
+#' \describe{
+#'   \item{geo}{country, area or territory (ISO-3)}
+#'   \item{t}{year}
+#'   \item{rank}{rank of country for given year}
+#'   \item{total}{overall index}
+#'   \item{code}{code of sub-indicator}
+#'   \item{indicator}{sub-indicator name}
+#'   \item{v}{value}
+#' }
+#'
+#' @details
+#'
+#' See https://fragilestatesindex.org/indicators for a description of each
+#' sub-indicator.
+#'
+#' Last updated 15 May 2024.
+#'
+#' @source The Fund for Peace
+"fsi"
