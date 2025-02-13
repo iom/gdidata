465d41e86235ca5d7a07ff50b8577c24090ba502
Kenneth S. Reyes <kenneth.rejes@gmail.com>
2025-02-13 10:45:53 +0100

    add `irreg_darien_*`

---
+++ R/irreg_darien_dmg.R
@@ -0,0 +1,22 @@
+#' Irregular Transits Through Darien: Demographics
+#'
+#' Monthly count of irregular transits through the Darien region of Panama, by
+#' sex and age See `irreg_darien_nat` for the same dataset with breakdown by
+#' nationality.
+#'
+#' \describe{
+#'   \item{t}{month-year of count}
+#'   \item{q}{quarter-year of count}
+#'   \item{demog}{sex and age category}
+#'   \item{n}{count of individuals}
+#' }
+#'
+#' @details Downloaded from https://www.migracion.gob.pa/estadisticas. Period
+#'   covered is currently Jan 2021 to Dec 2024. From 2021 to 2022, demographic
+#'   categories are only "men" and "women". From 2023 onwards, categories are
+#'   "men", "women", "boys" and "girls".
+#'
+#'   Data as of 31 December 2024. Last updated 13 February 2025.
+#'
+#' @source Panama Servicio Nacional de Migración \[National Migration Service\].
+"irreg_darien_nat"
---
+++ R/irreg_darien_nat.R
@@ -0,0 +1,20 @@
+#' Irregular Transits Through Darien: Nationality
+#'
+#' Monthly count of irregular transits through the Darien region of Panama, by
+#' nationality. See `irreg_darien_dmg` for the same dataset with breakdown by
+#' sex and age.
+#'
+#' \describe{
+#'   \item{t}{month-year of count}
+#'   \item{q}{quarter-year of count}
+#'   \item{nat}{nationality (ISO-3)}
+#'   \item{n}{count of individuals}
+#' }
+#'
+#' @details Downloaded from https://www.migracion.gob.pa/estadisticas. Period
+#'   covered is currently 2010-2019 (annual) and Jan 2020 to Dec 2024.
+#'
+#'   Data as of 31 December 2024. Last updated 12 February 2025.
+#'
+#' @source Panama Servicio Nacional de Migración \[National Migration Service\].
+"irreg_darien_nat"
