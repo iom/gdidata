ce8f9c0d8055b6e5a94e14861ed04ef1ea9e8503
Kenneth S. Reyes <kenneth.rejes@gmail.com>
2025-03-11 11:14:32 +0100

    update `irreg_honduras` and add `irreg_honduras_*`

--- R/irreg_honduras.R
+++ R/irreg_honduras.R
@@ -12,7 +12,49 @@
 #'   Migratorio Irregular* dashboard
 #'   (https://app.powerbi.com/view?r=eyJrIjoiODNhMGY4MGUtM2NkMy00YTIzLTlmZTAtNmQxZmJiZDhiZjc1IiwidCI6ImUxMWQ5NjIwLTRkM2UtNDEwYi05MTUyLWZkMWNmNmNmNzI5YSJ9).
 #'
-#'   Last updated 10 Mar 2025.
+#'   Last updated 11 Mar 2025.
 #'
 #' @source Honduras National Institute for Migration; DTM Americas.
 "irreg_honduras"
+
+
+#' Irregular Migrant Arrivals to Honduras: Nationalities
+#'
+#' Irregular migrant arrivals to Honduras as reported by the government, by
+#' nationality.
+#'
+#' \describe{
+#'   \item{t}{year of count}
+#'   \item{q}{quarter-year of count}
+#'   \item{n}{count of individuals}
+#' }
+#'
+#' @details Scraped from the Honduras National Institute for Migration's *Flujo
+#'   Migratorio Irregular* dashboard
+#'   (https://app.powerbi.com/view?r=eyJrIjoiODNhMGY4MGUtM2NkMy00YTIzLTlmZTAtNmQxZmJiZDhiZjc1IiwidCI6ImUxMWQ5NjIwLTRkM2UtNDEwYi05MTUyLWZkMWNmNmNmNzI5YSJ9).
+#'
+#'   Last updated 11 Mar 2025.
+#'
+#' @source Honduras National Institute for Migration.
+"irreg_honduras_nat"
+
+
+#' Irregular Migrant Arrivals to Honduras: Demographics
+#'
+#' Irregular migrant arrivals to Honduras as reported by the government, by sex
+#' and age.
+#'
+#' \describe{
+#'   \item{t}{year of count}
+#'   \item{q}{quarter-year of count}
+#'   \item{n}{count of individuals}
+#' }
+#'
+#' @details Scraped from the Honduras National Institute for Migration's *Flujo
+#'   Migratorio Irregular* dashboard
+#'   (https://app.powerbi.com/view?r=eyJrIjoiODNhMGY4MGUtM2NkMy00YTIzLTlmZTAtNmQxZmJiZDhiZjc1IiwidCI6ImUxMWQ5NjIwLTRkM2UtNDEwYi05MTUyLWZkMWNmNmNmNzI5YSJ9).
+#'
+#'   Last updated 11 Mar 2025.
+#'
+#' @source Honduras National Institute for Migration.
+"irreg_honduras_dmg"
